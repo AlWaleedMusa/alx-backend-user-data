49191a16f6a7149b83053b7afed0a9355197315a
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-29 18:42:44 +0300

    add: task 2 done, fixed the class and implemented format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -10,6 +11,24 @@ def filter_datum(
 ) -> str:
     """Returns a log message obfuscated"""
     for f in fields:
-        message = re.sub(f"{f}=.*?{separator}",
-                         f"{f}={redaction}{separator}", message)
+        message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log with filter_datum"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
