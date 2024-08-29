4b2e139eb572b6aad10bca08836a3a5a8d889816
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-29 14:46:34 +0300

    fix: task filtered_logger pip8

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,15 +7,6 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """
     Replaces occurrences of specified fields in a log message with a redaction string.
-
-    Args:
-        fields (list of str): List of field names to be redacted.
-        redaction (str): The string to replace the field values with.
-        message (str): The log message containing the fields to be redacted.
-        separator (str): The character that separates the field names and their values in the message.
-
-    Returns:
-        str: The log message with the specified fields redacted.
     """
     return re.sub(
         rf"({'|'.join(fields)})=.+?{separator}", rf"\1={redaction}{separator}", message
