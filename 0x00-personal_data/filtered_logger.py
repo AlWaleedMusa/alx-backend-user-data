a5bfef276cda661c20f8f3209c2b71709d2b3d9f
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-29 18:28:01 +0300

    fix: task filtered_logger refactored code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,13 @@
 """Module for filtering sensitive information from log messages."""
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Replaces occurrences of specified fields in a log message with a redaction string.
-    """
-    return re.sub(
-        rf"({'|'.join(fields)})=.+?{separator}", rf"\1={redaction}{separator}", message
-    )
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """Returns a log message obfuscated"""
+    for f in fields:
+        message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
+    return message
