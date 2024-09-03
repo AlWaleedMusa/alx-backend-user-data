9ff7a8fe522ef37cf416a2960a63f2db116520c1
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-04 02:28:22 +0300

    fix: task 7 pip8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,25 +7,29 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Authentication Class """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
 
         Args:
-            authorization_header (str): The Authorization header containing 'Basic <Base64-encoded string>'.
+            authorization_header (str): The Authorization
+            header containing 'Basic <Base64-encoded string>'.
 
         Returns:
             str: The Base64-encoded part of the Authorization header.
-                 Returns None if the header is invalid or not properly formatted.
+                 Returns None if the header is invalid
+                 or not properly formatted.
         """
 
         if not authorization_header:
             return None
-        
+
         if not isinstance(authorization_header, str):
             return None
-        
+
         if not authorization_header.startswith("Basic "):
             return None
-        
+
         return authorization_header[6:]
