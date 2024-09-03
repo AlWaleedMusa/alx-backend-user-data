6cb97a07b9442d543f44d35898c691ec62b7589b
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-04 02:15:43 +0300

    add: task 6 done

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""BasicAuth class"""
+
+from auth import Auth
+
+class BasicAuth(Auth):
+    """
+    A class used to handle authentication-related tasks inheriting from Auth.
+    """
+    pass
